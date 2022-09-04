8f855bafd8ae42512d6a99f512f4c1cfabe6a3a5
leofiemen <112003931+leofiemen@users.noreply.github.com>
2022-09-04 16:54:58 -0400

    update smile to smiley

---
+++ js/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(800,600);
+    
+    background(60, 189, 201)
+   
+    strokeWeight(10);
+    stroke(65, 60, 201)
+    fill(7, 252, 3);
+    circle(100,100, 100);
+
+    stroke(252, 252, 3)
+    strokeWeight(50);
+    fill(201, 60, 83);
+    rect(100, 200, 300, 200);
+    
+}
+function draw() {
+
+}
